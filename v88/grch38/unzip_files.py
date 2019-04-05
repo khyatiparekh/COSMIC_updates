350cee064e62d5bf19aa2990be14a4ec17d90a40
Khyati Parekh <khyatijparekh@gmail.com>
2019-04-05 13:56:35 -0700

    Create unzip_files.py

---
+++ v88/grch38/unzip_files.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import os
+import gzip
+import shutil
+
+def unzip_files():
+	files = os.listdir(os.path.abspath('./v88/grch38'))
+	print(files)
+	gziped = []
+	for i in files:
+		if '.gz' in i:
+			gziped.append(i)
+
+	print(gziped)
+	for i in gziped:
+		path = os.path.abspath('./v88/grch38') + '/' + i
+		fout = path.replace('.gz','')
+		with gzip.open(os.path.abspath('./v88/grch38')+'/'+i,'rb') as f_in:
+			with open(fout,'wb') as f_out:
+				shutil.copyfileobj(f_in,f_out)
+		print(i+' converted')
+		#os.remove(i)
+
+if __name__ == '__main__':
+	unzip_files()
